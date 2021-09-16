88682b4f71f77c9f32ff88deba42c1711966b2b2
nikhilchandrawar <89628390+nikhilchandrawar@users.noreply.github.com>
2021-09-16 15:43:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,8 @@
 const express = require('express');
-const path = require('path');
-const store = require('./store/datastore');
-const initialStoreData = require('./store/data');
-const Musician = require('./models/musician');
-const musicianRoutes = require('./routes/musician');
-
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 8080;
 
-// include routes
-app.use('/musician', musicianRoutes);
+app.get('/', (req, res) => res.send('Hello Nikhil. Happy Learning!'));
 
-app.use(express.static('public'));
-
-// Index route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
-
-// initialize store
-const musician = new Musician(store);
-musician.initStore(initialStoreData);
-app.locals.musician = musician;
-
-// start server
-const server = app.listen(port, () => {
-  console.log("Server started on port " + port);
-});
-
-module.exports = server;
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
